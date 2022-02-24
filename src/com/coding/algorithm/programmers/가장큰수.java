6e94ba9db1a8df045518e89f862a466ca0d7a2d7
Y.Hun <huyeon123@naver.com>
2022-02-24 11:16:14 +0900

    Sort, Succeed

---
+++ src/com/coding/algorithm/programmers/가장큰수.java
@@ -0,0 +1,25 @@
+package com.coding.algorithm.programmers;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 가장큰수 {
+    /**
+     *
+     */
+    public static void main(String[] args) {
+        가장큰수 T = new 가장큰수();
+        int[] numbers = {6, 10, 3};
+
+        System.out.print(T.solution(numbers));
+    }
+
+    public String solution(int[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((s1, s2) -> (s2 + s1).compareTo(s1 + s2)) //합쳐서 비교 후 내림차순 정렬
+                .reduce("", (s1, s2) -> s1.equals("0") && s2.equals("0") ? "0" : s1.concat(s2));
+    }
+
+
+}
